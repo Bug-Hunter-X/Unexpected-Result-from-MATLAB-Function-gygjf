d21c873bac3eb4321215bf85da01636c6d46baf4
BugHunter-X <askzenai@gmail.com>
2025-01-21 14:00:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,12 @@
+function result = myFunction(input)
+  % Some code here...
+  if someCondition && anotherCondition
+    result = someValue;
+  else
+    result = anotherValue; 
+  end
+end
+
+% Example usage
+input = 10;
+result = myFunction(input);
